4685058605aa39f407a440c325f54c8a0b6fa190
Dan Obregon <dan.obregon@destpet.com>
2025-01-03 14:45:15 -0600

    default.vue: +color mode selector

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
-        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
-          >Dan Obregon</NuxtLink
-        >
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+            >Dan Obregon</NuxtLink
+          >
+        </div>
+        <Menu />
       </div>
-      <Menu />
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
     <main class="p-2 mt-10">
       <slot />
@@ -34,5 +39,8 @@ body {
   font-optical-sizing: auto;
   font-style: normal;
   font-variation-settings: "wdth" 100;
+}
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
