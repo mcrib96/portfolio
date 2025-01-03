01b53861ae869d212769b7d0e7995f4751599197
Dan Obregon <dan.obregon@destpet.com>
2025-01-03 11:54:54 -0600

    default.vue: default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+          >Dan Obregon</NuxtLink
+        >
+      </div>
+      <Menu />
+    </header>
+    <main class="p-2">
+      <slot />
+    </main>
+  </div>
+</template>
+<script setup>
+useHead({
+  titleTemplate: "%s - Dan Obregon",
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      link: "https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap",
+    },
+  ],
+});
+</script>
+<style>
+body {
+  font-family: "Noto Sans", serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  font-variation-settings: "wdth" 100;
+}
+</style>
