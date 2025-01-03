1a5024fc1d5471bffdd3cc8ca879f21323e45939
Dan Obregon <dan.obregon@destpet.com>
2025-01-03 11:55:02 -0600

    about.vue: about me page

---
+++ app/pages/about.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    This page is about me.
+    <button @click="enableLayout('default')">use default layout</button>
+    <button @click="enableLayout('another')">use custom layout</button>
+  </div>
+</template>
+<script setup>
+useSeoMeta({
+  title: "About me",
+  description: "This page tells a little bit about me",
+  ogTitle: "About me",
+  ogDescription: "This page tells a little bit about me",
+  ogImage: "[og:image]",
+  ogUrl: "[og:url]",
+  twitterTitle: "About me",
+  twitterDescription: "This page tell a little bit about me",
+  twitterImage: "[twitter:image]",
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
+// definePageMeta({
+//   layout: "another",
+// });
+function enableLayout(layout) {
+  setPageLayout(layout);
+}
+</script>
