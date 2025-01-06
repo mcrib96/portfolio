2d7bb3675dd29adeb27c3a053b10f608589afba5
Dan Obregon <dan.obregon@destpet.com>
2025-01-06 15:47:23 -0600

    about.vue: use md5 content

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,40 +1,5 @@
 <template>
-  <div>
-    This page is about me.
-    <button @click="enableLayout('default')">use default layout</button>
-    <button @click="enableLayout('another')">use custom layout</button>
-  </div>
+  <article>
+    <ContentDoc class="prose dark:prose-invert" path="/about" />
+  </article>
 </template>
-<script setup>
-useSeoMeta({
-  title: "About me",
-  description: "This page tells a little bit about me",
-  ogTitle: "About me",
-  ogDescription: "This page tells a little bit about me",
-  ogImage: "[og:image]",
-  ogUrl: "[og:url]",
-  twitterTitle: "About me",
-  twitterDescription: "This page tell a little bit about me",
-  twitterImage: "[twitter:image]",
-  twitterCard: "summary",
-});
-
-useHead({
-  htmlAttrs: {
-    lang: "en",
-  },
-  link: [
-    {
-      rel: "icon",
-      type: "image/png",
-      href: "/favicon.png",
-    },
-  ],
-});
-// definePageMeta({
-//   layout: "another",
-// });
-function enableLayout(layout) {
-  setPageLayout(layout);
-}
-</script>
