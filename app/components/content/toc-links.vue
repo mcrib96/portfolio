a1333345772a020874c81539aa4c8f0e7a52b3a5
Dan Obregon <dan.obregon@destpet.com>
2025-01-07 14:31:32 -0600

    toc-links.vue: TOC links

---
+++ app/components/content/toc-links.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="link in links" :key="link.id">
+      <NuxtLink
+        :to="{ path: route.path, hash: `#${link.id}` }"
+        :class="{
+          'ml-4': level,
+          'text-green-400 dark:text-green-400': link.id === activeId,
+        }"
+      >
+        {{ link.text }}
+      </NuxtLink>
+      <TocLinks
+        :links="link.children"
+        :level="level + 1"
+        :active-id="activeId"
+      />
+    </li>
+  </ul>
+</template>
+<script setup>
+defineProps({
+  links: Array,
+  level: {
+    type: Number,
+    default: 0,
+  },
+  activeId: {
+    type: String,
+    default: null,
+  },
+});
+const route = useRoute();
+</script>
