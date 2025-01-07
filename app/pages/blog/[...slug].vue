c11df91a45da8a1e49693c24c7a4f5dbbfe5c6ee
Dan Obregon <dan.obregon@destpet.com>
2025-01-07 14:31:54 -0600

    [...slug].vue: add TOC if available

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,10 +1,45 @@
 <template>
-  <article class="prose dark:prose-invert">
-    <ContentDoc />
+  <article class="prose dark:prose-invert max-w-none">
+    <ContentDoc v-slot="{ doc }">
+      <div class="grid grid-cols-6 gap-16">
+        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+          <ContentRenderer :value="doc" />
+        </div>
+        <div class="col-span-2 not-prose" v-if="doc.toc">
+          <aside class="sticky top-8">
+            <div class="font-semibold mb-2">Table of Contents</div>
+            <nav>
+              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+            </nav>
+          </aside>
+        </div>
+      </div>
+    </ContentDoc>
   </article>
 </template>
 
 <script setup>
-const route = useRoute();
-console.log(route.params.slug);
+const activeId = ref(null);
+onMounted(() => {
+  const cb = (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id;
+      }
+    }
+  };
+  const observer = new IntersectionObserver(cb, {
+    threshold: 0.5,
+    root: null,
+  });
+  const elements = document.querySelectorAll("h2, h3");
+  for (const element of elements) {
+    observer.observe(element);
+  }
+  onBeforeUnmount(() => {
+    for (const element of elements) {
+      observer.unobserve(element);
+    }
+  });
+});
 </script>
