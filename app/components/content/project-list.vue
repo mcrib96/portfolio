a1e718a4cddbbe4737355cd72b6924fdecbcd5ab
Dan Obregon <dan.obregon@destpet.com>
2025-01-08 11:55:30 -0600

    project-list: dark mode fixes

--- app/components/content/project-list.vue
+++ app/components/content/project-list.vue
@@ -7,7 +7,7 @@
         <li
           v-for="repo in repos"
           :key="repo.id"
-          class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+          class="border border-gray-200 dark:border-gray-800 rounded-sm p-4 hover:bg-gray-200 font-mono dark:hover:bg-gray-800"
         >
           <a :href="repo.html_url" target="_blank">
             <div class="flex items-center justify-between text-sm">
