226269582fc902fff7d289d087a71e087ea4a5bf
Dan Obregon <dan.obregon@destpet.com>
2025-01-08 14:08:08 -0600

    latest-blog-posts.vue: handle limit here

--- app/components/content/latest-blog-posts.vue
+++ app/components/content/latest-blog-posts.vue
@@ -1,15 +1,24 @@
 <template>
-  <BlogPosts v-slot="{ posts }" :limit="limit">
-    <div v-for="post in posts" :key="post._path">
+  <BlogPosts v-slot="{ posts }">
+    <div v-for="post in getPosts(posts)" :key="post._path">
       <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
     </div>
   </BlogPosts>
 </template>
 <script setup>
-defineProps({
+const props = defineProps({
   limit: {
     type: Number,
     default: null,
   },
 });
+function getPosts(posts) {
+  const shortPosts = [];
+
+  if (props.limit && posts.length > props.limit) {
+    return posts.slice(0, props.limit);
+  } else {
+    return posts;
+  }
+}
 </script>
